cb63d185084192bd144750176a415e77dbe12736
Nikita Filipov <nikitafilipov.work@gmail.com>
2026-09-20 12:29:25 +0300

    Fix some bugs from Luke

--- services/user.py
+++ services/user.py
@@ -6,25 +6,17 @@ from db.models import User
 def create_user(
     username: str,
     password: str,
-    email: str | None = None,
-    first_name: str | None = None,
-    last_name: str | None = None,
+    email: str = "",
+    first_name: str = "",
+    last_name: str = "",
 ) -> User:
-    new_user = User.objects.create_user(
+    return get_user_model().objects.create_user(
         username=username,
         password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
     )
-    if email:
-        new_user.email = email
-
-    if first_name:
-        new_user.first_name = first_name
-
-    if last_name:
-        new_user.last_name = last_name
-
-    new_user.save()
-    return new_user
 
 
 def get_user(user_id: int) -> User:
